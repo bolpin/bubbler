405aaa4ed0560e5bc8b9856e22cc86c9561174d4
Brian Olpin <bolpin@gmail.com>
2016-02-18 07:20:50 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Bubbler - Raspberry Pi Passive-Infrared-Sensor-driven sound player
+
+To play sounds through the headphone jack (rather than via HDMI):
+'''
+sudo amixer cset numid=3 1
+'''
+(1 selects headphone socket.)
+
+See http://www.raspberrypi.org/learning/parent-detector/worksheet for a similar use of PIR sensors.
---
+++ main.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+import pygame
+
+pygame.mixer.init()
+pygame.mixer.music.load("bubbling_water.mp3")
+
+sensor = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
+
+previous_state = False
+current_state = False
+
+while True:
+	time.sleep(0.2)
+	#print("- -  pin %s is %s" % (sensor, GPIO.input(sensor)))
+	previous_state = current_state
+	current_state = GPIO.input(sensor)
+    
+	if current_state != previous_state:
+		new_state = "HIGH" if current_state else "LOW"
+		print("GPIO pin %s is %s" % (sensor, new_state))
+        if current_state == 1 :
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy() == True:
+                continue
